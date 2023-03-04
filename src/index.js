2543ac7cbf3315b773e41286d63ddc3bf3f695b2
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-03-04 16:32:24 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import './style.css';
+import "./style.css";
 
 import {
   createNewTask,
   deleteTask,
   loadTasks,
   titleElement,
-} from './modules/tasks.js';
+} from "./modules/tasks.js";
 
-const input = document.querySelector('#new-task-input');
-const submitButton = document.querySelector('.add');
-const taskList = document.querySelector('#tasks');
+const input = document.querySelector("#new-task-input");
+const submitButton = document.querySelector(".add");
+const taskList = document.querySelector("#tasks");
 loadTasks();
 
 // Add task
 submitButton.onclick = (e) => {
   e.preventDefault();
-  if (input.value !== '') {
+  if (input.value !== "") {
     createNewTask(input.value);
-    input.value = '';
+    input.value = "";
   }
 };
 
-taskList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('del')) {
+taskList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("del")) {
     const { id } = e.target.parentElement.dataset;
     deleteTask(id);
     e.target.parentElement.remove();
