97176d3eb8e9a8346c3ce18f8c0ef2bd4ac92e3f
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-05-06 17:37:07 +0300

    Update the DOM element

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
+// import './style.css';
+
+// import {
+//   createNewTask,
+//   deleteTask,
+//   loadTasks,
+//   titleElement,
+// } from './modules/tasks.js';
+
+// const input = document.querySelector('#new-task-input');
+// const submitButton = document.querySelector('.add');
+// const taskList = document.querySelector('#tasks');
+// loadTasks();
+
+// // Add task
+// submitButton.onclick = (e) => {
+//   e.preventDefault();
+//   if (input.value !== '') {
+//     createNewTask(input.value);
+//     input.value = '';
+//   }
+// };
+
+// taskList.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('del')) {
+//     const { id } = e.target.parentElement.dataset;
+//     deleteTask(id);
+//     e.target.parentElement.remove();
+//   }
+//   titleElement.focus();
+// });
 import './style.css';
 
-import {
-  createNewTask,
-  deleteTask,
-  loadTasks,
-  titleElement,
-} from './modules/tasks.js';
+import { initializeTasks, createNewTask } from './modules/tasks.js';
 
 const input = document.querySelector('#new-task-input');
 const submitButton = document.querySelector('.add');
-const taskList = document.querySelector('#tasks');
-loadTasks();
 
-// Add task
+initializeTasks();
+
 submitButton.onclick = (e) => {
   e.preventDefault();
   if (input.value !== '') {
@@ -20,12 +45,3 @@ submitButton.onclick = (e) => {
     input.value = '';
   }
 };
-
-taskList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('del')) {
-    const { id } = e.target.parentElement.dataset;
-    deleteTask(id);
-    e.target.parentElement.remove();
-  }
-  titleElement.focus();
-});
--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,9 +1,31 @@
-let tasks = [];
 const taskList = document.querySelector('#tasks');
 const titleElement = document.createElement('span');
+let tasks = [];
+function createTitleElement(title) {
+  const element = document.createElement('span');
+  element.className = 'title';
+  element.contentEditable = true;
+  element.textContent = title;
+  return element;
+}
 
 function saveTasksToLocalStorage() {
   localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function handleTitleInput(e) {
+  const taskElement = e.target.parentElement;
+  const taskId = taskElement.dataset.id;
+  const task = tasks.find((task) => task.id.toString() === taskId.toString());
+  task.title = e.target.textContent;
+  saveTasksToLocalStorage();
+}
+
+function createDeleteButton() {
+  const element = document.createElement('span');
+  element.className = 'del';
+  element.appendChild(document.createTextNode('Delete'));
+  return element;
 }
 
 function addTaskToDOM(task) {
@@ -14,18 +36,10 @@ function addTaskToDOM(task) {
   }
   taskElement.dataset.id = task.id;
 
-  const titleElement = document.createElement('span');
-  titleElement.className = 'title';
-  titleElement.contentEditable = true;
-  titleElement.textContent = task.title;
-  titleElement.addEventListener('input', () => {
-    task.title = titleElement.textContent;
-    saveTasksToLocalStorage();
-  });
+  const titleElement = createTitleElement(task.title);
+  titleElement.addEventListener('input', handleTitleInput);
 
-  const deleteButton = document.createElement('span');
-  deleteButton.className = 'del';
-  deleteButton.appendChild(document.createTextNode('Delete'));
+  const deleteButton = createDeleteButton();
   taskElement.appendChild(titleElement);
   taskElement.appendChild(deleteButton);
   taskList.appendChild(taskElement);
@@ -33,13 +47,31 @@ function addTaskToDOM(task) {
 
 function loadTasks() {
   const data = localStorage.getItem('tasks');
-  if (data) {
-    const tasksFromStorage = JSON.parse(data);
-    tasksFromStorage.forEach(addTaskToDOM);
-    tasks = tasksFromStorage;
-  } else {
-    tasks = [];
+  tasks = data ? JSON.parse(data) : [];
+  tasks.forEach(addTaskToDOM);
+}
+
+function deleteTask(taskId) {
+  tasks = tasks.filter((task) => task.id.toString() !== taskId.toString());
+  saveTasksToLocalStorage();
+}
+
+function handleTaskListClick(e) {
+  if (e.target.classList.contains('del')) {
+    const taskId = e.target.parentElement.dataset.id;
+    deleteTask(taskId);
+    e.target.parentElement.remove();
   }
+  titleElement.focus();
+}
+
+function setupEventListeners() {
+  taskList.addEventListener('click', handleTaskListClick);
+}
+
+function initializeTasks() {
+  loadTasks();
+  setupEventListeners();
 }
 
 function createNewTask(taskText) {
@@ -50,15 +82,7 @@ function createNewTask(taskText) {
   };
   tasks.push(task);
   addTaskToDOM(task);
-
   saveTasksToLocalStorage();
 }
 
-function deleteTask(taskId) {
-  tasks = tasks.filter((task) => task.id.toString() !== taskId.toString());
-  saveTasksToLocalStorage();
-}
-
-export {
-  addTaskToDOM, createNewTask, deleteTask, loadTasks, titleElement,
-};
+export { initializeTasks, createNewTask, deleteTask };
